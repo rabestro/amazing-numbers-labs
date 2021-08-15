a549cac5319bb339f1ff6bb0ad3b784e31ed9ce4
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-16 00:09:15 +0300

    Add test for Buzz property

--- src/test/java/numbers/property/BuzzTest.java
+++ src/test/java/numbers/property/BuzzTest.java
@@ -1,13 +1,29 @@
 package numbers.property;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Given Buzz property")
 class BuzzTest {
+    private Property underTest;
 
-    @Test
-    void test1() {
+    @BeforeEach
+    void setUp() {
+        underTest = new Buzz();
     }
 
+    @DisplayName("given test() method")
+    @ParameterizedTest(name = "when number is \"{0}\" then \"{1}\"")
+    @CsvFileSource(resources = "/property/buzz.csv", numLinesToSkip = 1)
+    void buzzProperty(final BigInteger number, final boolean expected) {
+        final var actual = underTest.test(number);
+        assertEquals(expected, actual);
+    }
 }
