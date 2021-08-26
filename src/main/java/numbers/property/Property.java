5c854386af9ccdb2f167290afe016c804d4be076
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-08-26 20:42:51 +0300

    closed #12

--- src/main/java/numbers/property/Property.java
+++ src/main/java/numbers/property/Property.java
@@ -1,11 +1,12 @@
 package numbers.property;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public interface Property extends Predicate<Number> {
-    Pattern NATURAL_NUMBER = Pattern.compile("[+]?\\d*\\d");
+    Pattern NATURAL_NUMBER = Pattern.compile("[+]?(?!0+$)\\d*\\d");
 
     static boolean isNaturalNumber(final String value) {
         return NATURAL_NUMBER.matcher(value).matches();
@@ -19,9 +20,14 @@ public interface Property extends Predicate<Number> {
 
     @Override
     default boolean test(Number number) {
-        return number instanceof BigInteger
-                ? test((BigInteger) number)
-                : test(number.longValue());
+        if (number instanceof BigDecimal) {
+            final var bigDecimal = (BigDecimal) number;
+            return test(bigDecimal.toBigInteger());
+        }
+        if (number instanceof BigInteger) {
+            return test((BigInteger) number);
+        }
+        return test(number.longValue());
     }
 
     default boolean test(long number) {
